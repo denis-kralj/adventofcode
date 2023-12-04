85551e14570e1cd920a62ba5e0210ef6fce1e06b
Denis <denis-kralj@users.noreply.github.com>
2023-12-04 20:56:37 +0000

    Change input filename to conventional value Move limits value to global scope Use new util method Change casing of methods

--- 2023/02/main.go
+++ 2023/02/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
+	"aoc/utils"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -16,23 +15,17 @@ type Game struct {
 	greenCount int
 }
 
-func main() {
-	file := Getfile("input_2.txt")
-	defer file.Close()
+var limits = Game{blueCount: 14, greenCount: 13, redCount: 12}
 
-	sc := bufio.NewScanner(file)
-	limits := Game{}
-	limits.blueCount = 14
-	limits.greenCount = 13
-	limits.redCount = 12
+func main() {
+	inputLines, _ := utils.GetProblemLines()
 
 	var sum int = 0
 	var sum2 int = 0
-	for sc.Scan() {
-		text := sc.Text()
-		gameCandidate := Buildcandidate(text)
+	for _, text := range inputLines {
+		gameCandidate := BuildCandidate(text)
 		sum2 += gameCandidate.blueCount * gameCandidate.greenCount * gameCandidate.redCount
-		if Isvalidcandidate(gameCandidate, limits) {
+		if IsValidCandidate(gameCandidate) {
 			sum += gameCandidate.gameNumber
 		}
 	}
@@ -41,33 +34,33 @@ func main() {
 	fmt.Println(sum2)
 }
 
-func Isvalidcandidate(candidate Game, limits Game) bool {
+func IsValidCandidate(candidate Game) bool {
 	return candidate.blueCount <= limits.blueCount &&
 		candidate.redCount <= limits.redCount &&
 		candidate.greenCount <= limits.greenCount
 }
 
-func Buildcandidate(input string) Game {
+func BuildCandidate(input string) Game {
 	candidate := Game{}
 	gameResults := strings.Split(input, ":")
 	gameNumber := strings.Split(gameResults[0], " ")[1]
-	candidate.gameNumber = Getnumberfromstring(gameNumber)
-	candidate.blueCount = Getmaxcountforcolor(gameResults[1], "blue")
-	candidate.redCount = Getmaxcountforcolor(gameResults[1], "red")
-	candidate.greenCount = Getmaxcountforcolor(gameResults[1], "green")
+	candidate.gameNumber = GetNumberFromString(gameNumber)
+	candidate.blueCount = GetMaxCountForColor(gameResults[1], "blue")
+	candidate.redCount = GetMaxCountForColor(gameResults[1], "red")
+	candidate.greenCount = GetMaxCountForColor(gameResults[1], "green")
 
 	return candidate
 }
 
-func Getmaxcountforcolor(gamesPlayed string, color string) int {
+func GetMaxCountForColor(gamesPlayed string, color string) int {
 	games := strings.Split(gamesPlayed, ";")
 	var highestCount int = 0
 	for i := 0; i < len(games); i++ {
 		draws := strings.Split(games[i], ",")
 		for j := 0; j < len(draws); j++ {
 			draw := strings.Split(strings.TrimSpace(draws[j]), " ")
-			if draw[1] == color && Getnumberfromstring(draw[0]) > highestCount {
-				highestCount = Getnumberfromstring(draw[0])
+			if draw[1] == color && GetNumberFromString(draw[0]) > highestCount {
+				highestCount = GetNumberFromString(draw[0])
 			}
 		}
 	}
@@ -75,7 +68,7 @@ func Getmaxcountforcolor(gamesPlayed string, color string) int {
 	return highestCount
 }
 
-func Getnumberfromstring(input string) int {
+func GetNumberFromString(input string) int {
 	number, err := strconv.Atoi(input)
 
 	if err != nil {
@@ -83,14 +76,4 @@ func Getnumberfromstring(input string) int {
 	}
 
 	return number
-}
-
-func Getfile(path string) *os.File {
-	file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return file
 }
