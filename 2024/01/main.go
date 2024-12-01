8a61d443fd2ed33cb2d1c7fbf7431a203b142655
Denis Kralj <denis-kralj@users.noreply.github.com>
2024-12-01 23:06:51 +0100

    Add solution for day 01 2024

---
+++ 2024/01/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"aoc/utils"
+	"fmt"
+	"log"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputLines, _ := utils.GetProblemLines()
+
+	firstList := []int{}
+	secondList := []int{}
+	simMap := make(map[int][2]int)
+
+	for _, text := range inputLines {
+		stringNumberPair := strings.Split(text, "   ")
+
+		firstList = append(firstList, GetNumberFromString(stringNumberPair[0]))
+		secondList = append(secondList, GetNumberFromString(stringNumberPair[1]))
+	}
+
+	slices.Sort(firstList)
+	slices.Sort(secondList)
+
+	sum := 0
+
+	for i := 0; i < len(firstList); i++ {
+		if firstList[i] > secondList[i] {
+			sum += firstList[i] - secondList[i]
+		} else {
+			sum += secondList[i] - firstList[i]
+		}
+		if value, ok := simMap[firstList[i]]; ok {
+			simMap[firstList[i]] = [2]int{value[0] + 1, value[1]}
+		} else {
+			simMap[firstList[i]] = [2]int{1, 0}
+		}
+
+		if value, ok := simMap[secondList[i]]; ok {
+			simMap[secondList[i]] = [2]int{value[0], value[1] + 1}
+		} else {
+			simMap[secondList[i]] = [2]int{0, 1}
+		}
+	}
+
+	simScore := 0
+
+	for key, value := range simMap {
+		simScore += key * value[0] * value[1]
+	}
+
+	fmt.Println("Total distance between lists: ", sum)
+	fmt.Println("Similarity score: ", simScore)
+}
+func GetNumberFromString(input string) int {
+	number, err := strconv.Atoi(input)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return number
+}
